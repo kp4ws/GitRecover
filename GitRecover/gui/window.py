6156abb5a0ed8d194bdc51cfcd2c9b589a22175f
kp4ws <kentpawson123@gmail.com>
2026-09-21 16:19:19 -0700

    Updated main window UI

--- GitRecover/gui/window.py
+++ GitRecover/gui/window.py
@@ -1,13 +1,15 @@
-from PySide6.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QWidget, QLabel, QStackedWidget, QHBoxLayout, QTextEdit, QMessageBox
+from PySide6.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QWidget, QLabel, QStackedWidget, QHBoxLayout, QLineEdit, QMessageBox, QFileDialog
 from PySide6.QtCore import QSize, Qt
 
 from GitRecover.ai.ollama_utils import provide_feedback
+
+from pathlib import Path
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Git Recovery Assistant")
-        self.setFixedSize(QSize(400, 300))
+        self.setFixedSize(QSize(480, 360))
 
         self.app_pages = QStackedWidget()
 
@@ -24,20 +26,46 @@ class MainWindow(QMainWindow):
         main_layout = QVBoxLayout(widget)
 
         browse_layout = QHBoxLayout()
-        git_repo_label = QLabel("Git repo to be recovered: ___________________")
-        repo_browse_btn = QPushButton("Browse")
-        browse_layout.addWidget(git_repo_label)
-        browse_layout.addWidget(repo_browse_btn)
+        git_repo_label = QLabel("Selected repository:")
+        git_repo_label.setStyleSheet("font-weight: bold;")
 
-        text_area = QTextEdit()
+        #By default, set git repo to current directory
+        cwd = Path.cwd()
+        git_repo_file = QLabel(f"{cwd}")
+        git_repo_file.setStyleSheet("color: gray; font-style: italic;")
+
+        repo_browse_btn = QPushButton("Browse")
+        repo_browse_btn.clicked.connect(lambda: self._handle_browse(git_repo_file))
+
+        # browse_layout.addWidget(git_repo_label)
+        browse_layout.addWidget(git_repo_file)
+        browse_layout.addWidget(repo_browse_btn)
+        browse_layout.addStretch()
+
+        prompt_label = QLabel("Enter details (optional)")
+        prompt_field = QLineEdit()
 
         git_recover_btn = QPushButton("Git Recover")
-        git_recover_btn.clicked.connect(lambda: self._handle_git_recover(text_area.toPlainText()))
+        git_recover_btn.clicked.connect(lambda: self._handle_git_recover(prompt_field.text()))
 
         main_layout.addLayout(browse_layout)
-        main_layout.addWidget(text_area)
+        main_layout.addWidget(prompt_label)
+        main_layout.addWidget(prompt_field)
         main_layout.addWidget(git_recover_btn)
+        main_layout.addStretch()
+
         return widget
+
+    def _handle_browse(self, file_label: QLabel):
+        directory = QFileDialog.getExistingDirectory(
+            self,
+            "Select your Git Repo",
+            dir=str(Path.cwd()),
+            options=QFileDialog.Option.ShowDirsOnly
+        )
+
+        if directory:
+            file_label.setText(directory)
 
     def _handle_git_recover(self, user_message: str):
         ai_response = provide_feedback(user_message)
@@ -47,5 +75,5 @@ class MainWindow(QMainWindow):
         dialog_response.setText(ai_response)
 
         ok_btn = dialog_response.exec()
-        if ok_btn == QMessageBox.Ok:
-            print("okay!")
+        # if ok_btn == QMessageBox.Ok:
+        #     print("okay!")
